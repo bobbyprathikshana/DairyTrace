9b3552157bf598a8fb3864fb62b2c5bbd030af8e
bobbyprathikshana <52849175+bobbyprathikshana@users.noreply.github.com>
2022-11-09 01:20:57 +0530

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,24 @@
 require('babel-register');
 require('babel-polyfill');
 
+
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { INFURA_API_KEY, MNEMONIC } = process.env;
+const AccountIndex = 0;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
-  },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
-  compilers: {
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, INFURA_API_KEY, AccountIndex);
+      },
+      network_id: "*"
     }
   }
-}
+};
