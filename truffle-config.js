b110baa069fe1f8c6f8622e7b72f98ddabfa8904
bobbyprathikshana <52849175+bobbyprathikshana@users.noreply.github.com>
2022-11-09 10:29:47 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -9,12 +9,7 @@ const AccountIndex = 0;
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
-    goerli: {
+     goerli: {
       provider: function() {
         return new HDWalletProvider(MNEMONIC, INFURA_API_KEY, AccountIndex);
       },
